e393d188dbb8603d22aa745981c37f91b7bcd3a0
Joe <Joe@nowhere.com>
2012-08-24 16:06:54 +0400

    added source array randdomization

--- main.cpp
+++ main.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
+#include <cstdlib>
 
 const int SIZE = 10;
 
 int* makeTestArray();
 
+int getRandom(int n);
+
 int main(int argc, char* argv[]) {
+  srand(time(NULL));
+
   int* data = makeTestArray();
   for (int i = 0; i < SIZE; i++)
     std::cout << data[i] << ",";
+  delete[] data;
   return 0;
 }
 
 
 int* makeTestArray() {
   int* data = new int[SIZE];
+  for (int i = 0; i < SIZE; i++)
+    data[i] = getRandom(100);
   return data;
+}
+
+int getRandom(int n) {
+  return rand() % n;
 }
