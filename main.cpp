6c13fb3e2d3b91cee2229f214e356f6dbeed0d8d
Bill <Bill@nowhere.com>
2012-08-24 13:36:57 +0400

    test data

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+const int size = 10;
+
+int* createData();
+
 int main(int argc, char* argv[]) {
-  
+  int* data = createData();
+  for (int i = 0; i < size; i++)
+    std::cout << data[i] << ",";
   return 0;
+}
+
+
+int* createData() {
+  int* data = new int[size];
+  return data;
 }
